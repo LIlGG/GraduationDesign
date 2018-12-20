2da605ac7b6f73b8d38c6db0a8aafa479f4101c5
LIlGG <1103069291@qq.com>
2018-12-20 14:49:29 +0800

    增加课程表文件

--- src/config/index.example.js
+++ src/config/index.example.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev'
 const domains = {
   dev: 'https://4e14ccec-abcc-46a7-a4fa-d99bd2a0cdd9.mock.pstmn.io', // 测试api接口
-  prod: 'https://api.com' // api接口
+  prod: 'http://localhost:8080' // api接口
 }
 export const domain = domains[env]
 export const version = '0.8.0' // 版本号
